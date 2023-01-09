dd893cc2ac6ba69a06c8fc076554b1e730f9ce86
Ayush Mishra <61145377+novaturient95@users.noreply.github.com>
2023-01-09 22:49:19 +0530

    update changes

--- tests/training/automl/tests/preprocessing/vision/__init__.py
+++ tests/training/automl/tests/preprocessing/vision/__init__.py
@@ -1,4 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Vision Test preprocessing init file"""
+"""Vision Test preprocessing init file."""
+
+from .prepare_classification_data import prepare_data as prepare_classification_data
+from .prepare_classification_multilabel_data import prepare_data as prepare_classification_multilabel_data
+from .prepare_instance_segmentation_data import prepare_data as prepare_instance_segmentation_data
+from .prepare_object_detection_data import prepare_data as prepare_object_detection_data
